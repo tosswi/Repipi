c0a37585c23cd03e5e5d41b5b28c56fab03a96ba
vagrant <vagrant@localhost.localdomain>
2020-05-09 03:13:25 +0000

    [add]recipe_index画像

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
     end
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:nickname,:sex,:allergies, :phone_number, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:nickname,:sex,:allergy, :phone_number, :image])
   end
   # データ更新時のパラメーターを設定する
   def configure_account_update_params
