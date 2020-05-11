b86900a08f7937a68bffaecb5600141adec8b8e7
tosswi <60781002+tosswi@users.noreply.github.com>
2020-05-12 08:21:37 +0900

    Update recipe_reviews_controller.rb

--- app/controllers/admins/recipe_reviews_controller.rb
+++ app/controllers/admins/recipe_reviews_controller.rb
@@ -2,11 +2,9 @@ class Admins::RecipeReviewsController < ApplicationController
   def destroy
     recipe_review=RecipeReview.find_by(params[:recipe_id])
     if  recipe_review.destroy
-        recipe_review.user.point -= 5
-        recipe_review.user.save
-        redirect_to admins_path
+        redirect_to request.referer
     else
-      redirect_to admins_path
+      redirect_to request.referer
     end
   end
 end
