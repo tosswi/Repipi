33148e087fa018cbc5af4664e15f043a5fe04eed
vagrant <vagrant@localhost.localdomain>
2020-05-14 03:15:47 +0000

    deploytest2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,24 +25,22 @@
 
 
 
-$(function(){
+$(function () {
   //querySelectorでfile_fieldを取得
   var file_field = document.querySelector('input[type=file]')
   //fileが選択された時に発火するイベント
-  $('#img-file').change(function(){
+  $('#img-file').change(function () {
     //選択したfileのオブジェクトをpropで取得
     var file = $('input[type="file"]').prop('files')[0];
     //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
     var fileReader = new FileReader();
     //読み込みが完了すると、srcにfileのURLを格納
-    fileReader.onloadend = function() {
+    fileReader.onloadend = function () {
       var src = fileReader.result
-      var html= `<img src="${src}" width="114" height="80">`
+      var html = `<img src="${src}" width="114" height="80">`
       //image_box__container要素の前にhtmlを差し込む
       $('#image-box__container').before(html);
     }
     fileReader.readAsDataURL(file);
   });
 });
-
-
